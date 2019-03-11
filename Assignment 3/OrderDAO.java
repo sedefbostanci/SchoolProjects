405492861beff8861ad6a30285c0d072c0f110be
sedefbostanci <48382473+sedefbostanci@users.noreply.github.com>
2019-03-11 15:22:59 +0300

    Delete OrderDAO.java

--- Assignment 3/OrderDAO.java
+++
@@ -1,9 +0,0 @@
-import java.util.List;
-
-public interface OrderDAO {
-	
-	public List<Customer> getAllOrder();
-	public void createOrder(int order, int Id);
-	public void removeOrder(int orderId);
-	public void removeCustomer(int customerId);
-}
