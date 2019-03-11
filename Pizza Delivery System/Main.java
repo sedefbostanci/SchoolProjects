abcd9d364feb1061d5ae5b7f62c2bd9e91927f09
sedefbostanci <48382473+sedefbostanci@users.noreply.github.com>
2019-03-11 15:20:32 +0300

    Add files via upload

---
+++ Pizza Delivery System/Main.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException{
+		
+		String dosyaName;
+		
+		
+		inputExecution input= new inputExecution("input.txt"); //Execute input txt file
+	}
+	
+	
+}
