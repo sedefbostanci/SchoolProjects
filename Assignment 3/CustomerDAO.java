e0c51e5af22ddb062514a7809a0c20e347eacfff
sedefbostanci <48382473+sedefbostanci@users.noreply.github.com>
2019-03-11 15:22:38 +0300

    Delete CustomerDAO.java

--- Assignment 3/CustomerDAO.java
+++
@@ -1,9 +0,0 @@
-import java.util.List;
-
-public interface CustomerDAO {
-	public List<Customer> getAll();
-	public void addCustomer(Customer customer);
-	public void deleteCustomer(int Id);
-	
-
-}
